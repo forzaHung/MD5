94211744fb2baceba1a41b9ad97b0e44b5322c27
forzaHung <vuxuanhungmta@gmail.com>
2018-04-05 07:24:38 +0700

    add project

---
+++ MD5/Common.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD5
+{
+    public class Common
+    {
+        public static string GetMD5(string chuoi)//khai báo thêm Static
+        {
+            string str_md5 = "";
+            byte[] mang = System.Text.Encoding.UTF8.GetBytes(chuoi);
+
+            MD5CryptoServiceProvider my_md5 = new MD5CryptoServiceProvider();
+            mang = my_md5.ComputeHash(mang);
+
+            foreach (byte b in mang)
+            {
+                str_md5 += b.ToString("x2");//Nếu là "X2" thì kết quả sẽ tự chuyển sang ký tự in Hoa
+            }
+
+            return str_md5;
+        }
+    }
+}
